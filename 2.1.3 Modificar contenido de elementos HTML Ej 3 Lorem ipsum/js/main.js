19c75af7351db1b1104351c7e58be1a0980f4482
Iván (profe) <ivan@adalab.es>
2024-11-21 21:01:19 +0100

    Ejercicio 3.

---
+++ 2.1.3 Modificar contenido de elementos HTML Ej 3 Lorem ipsum/js/main.js
@@ -0,0 +1,7 @@
+const h1 = '<h1>Lorem ipsum</h1>';
+
+const paragraph = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+const image = '<img src="http://via.placeholder.com/350x150" alt="Imagen de ejemplo con las medidas">';
+const div = document.querySelector('.js_div');
+
+div.innerHTML = h1 + paragraph + image;
